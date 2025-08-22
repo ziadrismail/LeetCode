2825c9f5a204ac6356af44c5d656d84123a16891
Ziad Reda <83479088+ziadrismail@users.noreply.github.com>
2025-08-22 15:51:13 +0300

    Update approach_2.java

--- 242. Valid Anagram/approach_2.java
+++ 242. Valid Anagram/approach_2.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int n = s.length(), m = t.length();
-
-        if (n != m) {
+        if (s.length() != t.length()) {
             return false;
         }
 
         int[] freq = new int[26];
-        for (int i = 0; i < n; ++i) {
-            char si = s.charAt(i), ti = t.charAt(i);
-            freq[si - 'a']++;
-            freq[ti - 'a']--;
+        for (int i = 0; i < s.length(); ++i) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
 
-        for (int i = 0; i < 26; ++i) {
-            if (freq[i] != 0) {
+        for (int val : freq) {
+            if (val != 0) {
                 return false;
             }
         }
