ec4355f6e088800f048c3550eccdfbba5a41dbf2
Ziad Reda <83479088+ziadrismail@users.noreply.github.com>
2025-08-24 18:37:02 +0300

    Create approach_2.java

---
+++ 746. Min Cost Climbing Stairs/approach_2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+
+        for (int i = 2; i < n; ++i) {
+            cost[i] += Math.min(cost[i - 1], cost[i - 2]);
+        }
+
+        return Math.min(cost[n - 1], cost[n - 2]);
+    }
+}
+
+/*
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
