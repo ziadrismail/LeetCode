d9d18b4236881d0ff6d9ae4e736cb12ce345f8af
Ziad Reda <83479088+ziadrismail@users.noreply.github.com>
2025-08-31 17:54:07 +0300

    Create approach_2.java

---
+++ 215. Kth Largest Element in an Array/approach_2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+}
+
+/*
+    Time Complexity: O(nlog(k))
+    Space Complexity: O(k)
+*/
